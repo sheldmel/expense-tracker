fd755ada9a220f619dcfd2e4193103c210fe729b
sheldmel <dmello.shelton@yahoo.com>
2026-05-10 11:03:20 +0530

    Chore: update API URL call on frontend

--- expense-tracker-frontend/src/services/api.js
+++ expense-tracker-frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api'
+    baseURL: import.meta.env.VITE_API_URL
 });
-
 api.interceptors.request.use(config => {
     const token = localStorage.getItem('token');
     if (token) config.headers.Authorization = `Bearer ${token}`;
