490e26927ed2cfdee5309d905699ea634a8f3a76
sheldmel <dmello.shelton@yahoo.com>
2026-05-15 10:30:01 +0530

    fix: update API base URL to use deployed environment variable

--- expense-tracker-frontend/src/services/api.js
+++ expense-tracker-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL_LOCAL
+    baseURL: import.meta.env.VITE_API_URL
 });
 api.interceptors.request.use(config => {
     const token = localStorage.getItem('token');
